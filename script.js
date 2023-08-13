3006871f51f48ab3fbcdfc5b619b68f9ec8f9ad5
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2023-08-13 12:17:59 -0400

    Add zero number and align UI

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let firstNumber, secondNumber, operator;
 
-
+// Addition, subtraction, multiplication, division functions
 const addition = function add(a, b) {
     return a + b;
 }
@@ -17,6 +17,7 @@ const division = function divide(a, b) {
     return a / b;
 }
 
+// operation function - takes operator and two numbers and returns operation
 const operation = function operate(operator, firstNumber, secondNumber) {
     if (operator == addition)
         return addition(firstNumber, secondNumber);
@@ -28,4 +29,20 @@ const operation = function operate(operator, firstNumber, secondNumber) {
         return division(firstNumber, secondNumber);
     else
         return "ERROR OPERATING";
+}
+
+// Populates display when number buttons are clicked 
+const numbers = document.querySelectorAll('.numbers');
+
+numbers.forEach((number) => {
+    number.addEventListener('click', display)
+})
+
+function display(e) {
+    const displayContainer = document.querySelector('.display-container');
+    const display = document.createElement('p');
+    display.classList.add('display');
+    display.textContent = e.target.textContent;
+
+    displayContainer.appendChild(display);
 }
